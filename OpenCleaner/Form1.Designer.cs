4ba0cee9b828af4c52100dd5f53e040901359a42
Daniel Jones <danielreddjones@gmail.com>
2022-12-12 18:45:47 -0600

    Added ability to test if it is running as administrator. Changed verification prompt.

--- OpenCleaner/Form1.Designer.cs
+++ OpenCleaner/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.InstLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -52,11 +54,33 @@
             this.InstLabel.Text = "Press Optimize to begin the process";
             this.InstLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Yes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(549, 372);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "No";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // OpenCleaner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.InstLabel);
             this.Controls.Add(this.button1);
             this.Name = "OpenCleaner";
@@ -69,5 +93,7 @@
 
         private Button button1;
         private Label InstLabel;
+        private Button button2;
+        private Button button3;
     }
 }
--- OpenCleaner/Form1.cs
+++ OpenCleaner/Form1.cs
@@ -1,23 +1,73 @@
+/*
+ * Author: Daniel Jones
+ * Operating system: Windows 10 Pro 10.0.19045
+ * Last modified: 12/12/2022
+ * IDE/.net: Visual Studio 2022 .net 6.0
+ * Purpose: Checks for common optimization configurations and changes upon user request.
+ */
+
+
+
+using System.Security.Principal;
+
 namespace OpenCleaner
 {
     public partial class OpenCleaner : Form
     {
         //counter variable
         int i = 0;
+        bool TruFls;
         public OpenCleaner()
         {
             InitializeComponent();
-        }
+            if (IsAdministrator() == false)
+            {
+                InstLabel.Text = "Must be run as administrator.";
+                button1.Visible = false;
+            }
+            
+            }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            button1.Visible = false;
 
-            i = i + 1;
-            if (i == 1) {
-                InstLabel.Text = "Opening Task Manager.\n Choose what startup programs that you want to disable.";
-            } 
-
+            CleanupMethod();
             
+        }
+
+        private void CleanupMethod()
+        {
+            i = i + 1;
+            if (i == 1)
+            {
+                InstLabel.Text = "Do you wish to disable all startup programs? Press yes to disable all startup programs and no to open task manager startup programs tag.";
+
+                if (TruFls == true)
+                {
+                    InstLabel.Text = "disabling startup programs.";
+
+                }
+            }
+
+        }
+
+        //Checks if program is being run as administrator. Returns true/false.
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TruFls = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TruFls = false;
         }
     }
 }
